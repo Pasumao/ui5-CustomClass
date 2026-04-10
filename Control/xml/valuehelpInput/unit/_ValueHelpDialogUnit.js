cb3a7bd31debfa2c8d4ec6e0d1bf656b7fb23222
Pasumao <1830316810@qq.com>
2026-04-10 09:59:24 +0800

    现在在valuehelp页面的search里回车即可触发search

--- Control/xml/valuehelpInput/unit/_ValueHelpDialogUnit.js
+++ Control/xml/valuehelpInput/unit/_ValueHelpDialogUnit.js
@@ -182,9 +182,8 @@ sap.ui.define([
     ValueHelpDialog.prototype._getFilterBar = function (aColumnConfig) {
         const oFilterBar = new FilterBar({
             basicSearch: new SearchField({
-                search: oEvent => {
-                    this._oDialog.getFilterBar().fireSearch();
-                }
+                search: oEvent => { this._oDialog.getFilterBar().fireSearch() },
+                change: oEvent => { this._oDialog.getFilterBar().fireSearch() }
             }),
             filterGroupItems: aColumnConfig
                 .map(({ text, key, visibleInFilterBar, hiddenFilter }) => {
@@ -201,7 +200,12 @@ sap.ui.define([
                                     supportRanges: true,
                                     supportRangesOnly: true,
                                     key: "range"
-                                })
+                                }),
+                                tokenUpdate: oEvent => {
+                                    setTimeout(() => {
+                                        this._oDialog.getFilterBar().fireSearch()
+                                    }, 0);
+                                }
                             });
                             oMultiInput.addValidator(ValueHelpDialog._Validator("range"));
                             return oMultiInput;
